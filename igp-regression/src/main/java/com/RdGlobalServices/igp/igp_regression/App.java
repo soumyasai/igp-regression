dd06ad3de24070a992b6174572973417487d9594
sai <sai02040@gmail.com>
2018-08-07 17:57:41 +0530

    first commit

---
+++ igp-regression/src/main/java/com/RdGlobalServices/igp/igp_regression/App.java
@@ -0,0 +1,13 @@
+package com.RdGlobalServices.igp.igp_regression;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
